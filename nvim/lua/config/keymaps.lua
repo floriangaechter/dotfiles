feca21dcd4679d101a608637464e63026dd8307c
Florian Gächter <flo@gaechter.xyz>
2024-01-18 09:34:55 +0100

    feat: add ; and , as escape sequenes when in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("i", "jj", "<ESC>", { silent = true })
-vim.keymap.set("n", "∆", ":m+<CR>", { silent = true })
-vim.keymap.set("n", "˚", ":m-2<CR>", { silent = true })
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", ";;", "<Esc>A;<Esc>")
+vim.keymap.set("i", ",,", "<Esc>A,<Esc>")
+vim.keymap.set("n", "∆", ":m+<Cr>")
+vim.keymap.set("n", "˚", ":m-2<Cr>")
