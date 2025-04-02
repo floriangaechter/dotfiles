920358d2be0c6b44a7477208cc99ffa576dbfef1
Florian Gächter <flo@gaechter.xyz>
2025-04-02 18:32:16 +0200

    fix: make script more portable

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 _get_battery_status() {
     local _current_status
     local _charging="Yes"
 
-    if [[ `uname` == 'Linux' ]]; then
+    if [[ $(uname) == 'Linux' ]]; then
         # Check if both battery files exist
         if [[ -f "/sys/class/power_supply/BAT0/capacity" && -f "/sys/class/power_supply/BAT1/capacity" ]]; then
             # Get capacity of both batteries
@@ -12,11 +12,11 @@ _get_battery_status() {
             local _bat1_capacity=$(cat /sys/class/power_supply/BAT1/capacity)
 
             # Average the capacities
-            _current_status=$(( (_bat0_capacity + _bat1_capacity) / 2 ))
+            _current_status=$(((_bat0_capacity + _bat1_capacity) / 2))
 
             # Check if either battery is charging
-            if [[ -f "/sys/class/power_supply/BAT0/status" && $(cat /sys/class/power_supply/BAT0/status) == "Discharging" ]] || \
-               [[ -f "/sys/class/power_supply/BAT1/status" && $(cat /sys/class/power_supply/BAT1/status) == "Discharging" ]]; then
+            if [[ -f "/sys/class/power_supply/BAT0/status" && $(cat /sys/class/power_supply/BAT0/status) == "Discharging" ]] ||
+                [[ -f "/sys/class/power_supply/BAT1/status" && $(cat /sys/class/power_supply/BAT1/status) == "Discharging" ]]; then
                 _charging="No"
             fi
         elif [[ -f "/sys/class/power_supply/BAT0/capacity" ]]; then
@@ -29,7 +29,7 @@ _get_battery_status() {
             [[ -f "/sys/class/power_supply/BAT1/status" && $(cat /sys/class/power_supply/BAT1/status) == "Discharging" ]] && _charging="No"
         fi
     else
-        local _battery_info=`ioreg -rc AppleSmartBattery`
+        local _battery_info=$(ioreg -rc AppleSmartBattery)
         _current_status=$(echo $_battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
         _charging=$(echo $_battery_info | grep -o '"ExternalConnected" = Yes' | awk '{print $3}')
     fi
