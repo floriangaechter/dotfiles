fead501c51718feb4de87b0b888f45d20beaf973
Florian Gächter <florian.gaechter@frontify.com>
2025-11-19 08:05:13 +0100

    fix treesitter config

--- nvim/lua/flo/plugins/treesitter.lua
+++ nvim/lua/flo/plugins/treesitter.lua
@@ -1,16 +1,14 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "vim", "vimdoc" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-    })
-  end,
+  opts = {
+    ensure_installed = { "lua", "vim", "vimdoc" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
