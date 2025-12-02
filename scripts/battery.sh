69c01511fb577b5c471e60b9ba79b66440a1f0db
Florian Gächter <flo@gaechter.xyz>
2025-12-02 14:58:57 +0100

    add linux settings

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -4,9 +4,16 @@ _get_battery_status() {
   local _current_status
   local _charging="Yes"
 
-  local _battery_info=$(ioreg -rc AppleSmartBattery)
-  _current_status=$(echo $_battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  _charging=$(echo $_battery_info | grep -o '"ExternalConnected" = Yes' | awk '{print $3}')
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    local _battery_info=$(upower -i /org/freedesktop/UPower/devices/DisplayDevice)
+    _current_status=$(echo $_battery_info | grep -o 'percentage' | awk '{printf "%.0f", $2}')
+    _charging=$(echo $_battery_info | awk '/state:/ {print ($2 == "charging" ? "Yes" : "")}')
+    echo $_current_status
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    local _battery_info=$(ioreg -rc AppleSmartBattery)
+    _current_status=$(echo $_battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+    _charging=$(echo $_battery_info | grep -o '"ExternalConnected" = Yes' | awk '{print $3}')
+  fi
 
   if [ "$_charging" = "Yes" ]; then
     printf ""
--- scripts/t.sh
+++ scripts/t.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Developer ~/Developer/private -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/Developer ~/Developer/private ~/Code -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
